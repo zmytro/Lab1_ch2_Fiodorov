42367bdf843f16480d9caeab4e176f250b0d799e
12233 <1223334444987kdarvis@gmail.com>
2021-09-11 02:59:31 +0300

    Test Commit3

--- src/com/company/n2_30.java
+++ src/com/company/n2_30.java
@@ -11,13 +11,13 @@ public class n2_30 {
         int d1,d2,d3,d4,d5;
         System.out.print("Enter number:");
         number = input.nextInt();
-        if(number / 10000 != 0) {
+        if((((number % 10000) % 1000 % 100)) % 10 == 5) {
             d1 = number / 10000;
             d2 = (number % 10000) / 1000;
             d3 = ((number % 10000) % 1000) / 100;
             d4 = (((number % 10000) % 1000 % 100)) / 10;
             d5 = (((number % 10000) % 1000 % 100)) % 10;
             System.out.print(d1 + " " + d2 + " " + d3 + " " + d4 + " " + d5);
-        }else System.out.print("The humber is too short");
+        }else System.out.print("The humber is too short/long");
     }
 }
